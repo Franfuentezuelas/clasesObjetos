4003a286b9c79df735485c83297e1a6170d69516
Phenix00069 <ftormol548@g.educaand.es>
2024-10-09 12:22:44 +0200

    clases e interface

--- clases/Producto.php
+++ clases/Producto.php
@@ -1,10 +1,24 @@
 <?php
 
+/**
+ * Clase Producto
+ */
 final class Producto{
 
-    public String "nombre";
+    /**
+     * @var String con el nombre del producto
+     */
+    public String $nombre;
 
-    
+    /**
+     * @param String $name variable que necesita el metodo contructor para crear un producto
+     * 
+     * @return none devuelve ya que instancia el objeto
+     */
+    public function __constructor(String $name){
+        $this -> nombre =$name;
+    };
+
 }
 
 
